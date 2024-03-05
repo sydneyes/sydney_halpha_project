2ddf10b5a3d0b6bc38446f1e84c669d910a1cee0
Lukas Bertoli <lukasbertoli@gmx.ch>
2024-03-05 09:35:47 +0100

    error handling

--- sun_catching/livestream.py
+++ sun_catching/livestream.py
@@ -13,27 +13,33 @@ from upload_image import run_smbclient
 cam_id = b"QHY5III200M-c8764d41ba464ec75"
 path = os.path.join(os.path.dirname(__file__), 'qhyccd.dll')
 cam_control = CameraControl(cam_id=cam_id, dll_path=path)
-
 #success = cam_control.cam.so.InitQHYCCDResource()
 n = 10
 exposure_time = np.linspace(50, 220, n)
 gain = 20
 offset = 6
 
+
 images = []
 for i in range(n):
-    image = cam_control.single_frame(exposure_time[i], gain, offset)
-    print(image)
-    images.append(image)
-cam_control.close()
-
-#Processing the images
-#First shifting the image
-shifted_images = alignment(images)
-
-# Secondly doing post processing and labelling the image
-text_image = image_processing(shifted_images)
-output_path = '/home/ubuntu/docs/halpha/Sun_catching/sun.PNG'
-cv2.imwrite(output_path, text_image)
-#Loading the images to the website
-run_smbclient()
+    try:
+        image = cam_control.single_frame(exposure_time[i], gain, offset)
+        print(image)
+        images.append(image)
+    except Exception as e:
+        cam_control.close()
+        print(f"An error occured with the camera: {e}")
+        print("Please go to the script that manages the camera:")
+        print("/home/pi/docs/halpha/sun_catching/CameraControl.py")
+     
+if len(images) == n:
+    #Processing the images
+    #First shifting the image
+    shifted_images = alignment(images)
+    if shifted_images != None:
+        # Secondly doing post processing and labelling the image
+        text_image = image_processing(shifted_images)
+        output_path = '/home/ubuntu/docs/halpha/sun_catching/sun.PNG'
+        cv2.imwrite(output_path, text_image)
+        #Loading the images to the websites
+        run_smbclient()
